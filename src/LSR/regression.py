fc09c14c4ea28787f6b3220523b987ea4be15f54
mcdebarros <mauricio.debarros@ucalgary.ca>
2023-04-17 18:14:37 -0600

    documentation for regression

--- src/LSR/regression.py
+++ src/LSR/regression.py
@@ -2,6 +2,26 @@ import numpy as np
 
 #Performs linear LSR to fit a model based on inputs
 def multi_regress(y,z):
+    """Perform multiple linear regression.
+
+    Parameters
+    ----------
+    y : array_like, shape = (n,1)
+        The vector of dependent variable data
+    Z : array_like, shape = (n,m)
+        The matrix of independent variable data
+
+    Returns
+    -------
+    a: numpy.ndarray, shape = (m,1)
+        The vector of model coefficients
+    e: numpy.ndarray, shape = (n,1)
+        The vector of residuals
+    rsq: float
+        R^2 value of model, coefficient of determination
+    model: numpy.ndarray, shape = (m,1)
+        Vector of model output
+"""
 
     ztz = np.matmul(np.transpose(z),z)
     zty = np.matmul(np.transpose(z),y)
